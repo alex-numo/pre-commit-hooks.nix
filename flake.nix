53117cbda17d2075f575d2e46bd1c2513f064c30
alex-numo <alex@numo.com>
2021-06-09 00:07:06 -0400

    allow aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    let systems = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
+    in
+    flake-utils.lib.eachSystem systems (system:
       let
         exposed = import ./nix { nixpkgs = nixpkgs; inherit system; gitignore-nix-src = null; isFlakes = true; };
       in
